17694d1a4a5445d5f2dc75c5438741de4cf587c4
Josie <jlderosier73@yahoo.com>
2021-02-11 16:19:01 -0800

    Add project files.

---
+++ Assets/Agent.cs
@@ -0,0 +1,23 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Agent : MonoBehaviour
+{
+    // Refers to the rigidbody that will be driven
+    // (assign this in the editor)
+    [SerializeField]
+    protected Rigidbody rbody;
+    
+    // Defines the change in the player's position over time
+    public virtual Vector3 velocity { get; set; }
+    
+    // Integrate movement and update the player's position
+    public virtual void UpdateMovement()
+    {
+        // Moves the rigidbody to the new position
+        //
+        // The new position is equal to newPos = pos + vel * dT
+        rbody.MovePosition(rbody.position + velocity * Time.deltaTime);
+    }
+}
