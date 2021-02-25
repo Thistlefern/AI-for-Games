8d5654b6917496fc838a2eb534c633da0a3cd440
Josie <jlderosier73@yahoo.com>
2021-02-24 17:45:17 -0800

    Trying to work through Wander and Pursue steering methods.

--- Assets/Scripts/AISteeringController.cs
+++ Assets/Scripts/AISteeringController.cs
@@ -2,6 +2,7 @@
 using System.Collections.Generic;
 using UnityEngine;
 
+
 public class AISteeringController : MonoBehaviour
 {
     public Agent agent;
@@ -37,6 +38,17 @@ public class AISteeringController : MonoBehaviour
         }
     }
 
+    public class WanderSteering : SteeringBehavior
+    {
+        public Vector3 target;
+        public override Vector3 Steer(AISteeringController controller)
+        {
+            return (controller.transform.position).normalized * controller.maxSpeed - target.normalized;
+        }
+    }
+
+
+
     // Add behaviors to this to consider them when calculating steering forces (This happens in Start() section)
     protected List<SteeringBehavior> steerings = new List<SteeringBehavior>();
 
@@ -58,18 +70,31 @@ public class AISteeringController : MonoBehaviour
     // and have each derived type have its own parameters for initializing behaviors.
     public Transform seekTarget;
     public Transform fleeTarget;
+    // Vector3 wanderTarget;
+    
     private void Start()
     {
-        steerings.Add(new SeekSteering { target = seekTarget });    // add SeekSteering to the 'steerings' list
-        steerings.Add(new FleeSteering { target = fleeTarget });
+        // add the different steering types to the 'steerings' list
+        steerings.Add(new SeekSteering { target = seekTarget });
+        // steerings.Add(new FleeSteering { target = fleeTarget });
+        // steerings.Add(new WanderSteering { target = wanderTarget});
+
     }
     private void Update()
     {
+        Vector3 lastLocation = seekTarget.position;
+
+
+        //wanderTarget = new Vector3(Random.insideUnitSphere.x, 0.0f, Random.insideUnitSphere.z);
+        //Debug.Log(wanderTarget);
+
         Vector3 steeringForce = CalculateSteeringForce();
         // Add steering force to velocity -- clamp its magnitude w/ respect to maxSpeed
         agent.velocity = Vector3.ClampMagnitude(agent.velocity + steeringForce,
                                                 maxSpeed);
         agent.UpdateMovement();
+
+        Debug.Log(seekTarget.transform);
     }
 }
 
