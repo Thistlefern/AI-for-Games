e9764116a757c9d4838b82e5d2da3b75769442b0
Josie <jlderosier73@yahoo.com>
2021-03-05 17:18:29 -0800

    Had a very slow day, worked on my behavior plan more than my code, will be working during my week again

---
+++ Assets/Scripts/DecisionTreeController.cs
@@ -0,0 +1,29 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class DecisionTreeController : MonoBehaviour
+{
+    public Agent agent;
+    public bool isPrey;
+    public bool isAlpha;
+    public float health;
+    public float fatigue;
+    public float thirst;
+    public float hunger;
+    public float perception;
+    public float sneak;
+
+    public IDecision decisionTreeRoot;
+
+    // set up your tree here, assigning additional decisions to the left and right of the tree
+    void Start()
+    {
+        decisionTreeRoot = new PrintDecision(true);
+    }
+
+    void Update()
+    {
+        decisionTreeRoot.MakeDecision();
+    }
+}
---
+++ Assets/Scripts/IDecision.cs
@@ -0,0 +1,40 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public interface IDecision
+{
+    IDecision MakeDecision();
+
+    // You may want to add properties to retrieve the left or right children of the decision, if you'd like
+}
+// The PrintDecision type implements the IDecision interface
+// - When a type implements an interface, it must define all members defined by the interface. This case, that's just the MakeDecision() method.
+public class PrintDecision : IDecision
+{
+    public bool branch = false;
+
+    // default constructor
+    public PrintDecision() { }
+
+    // parameterized constructor allows you to define branch on construction
+    public PrintDecision(bool branch)
+    {
+        this.branch = branch;
+    }
+
+    // evaluate the decision
+    public IDecision MakeDecision()
+    {
+        Debug.Log(branch ? "Yes" : "No");
+        return null;
+    }
+}
+
+public class FatigueCheck : IDecision
+{
+    public IDecision MakeDecision()
+    {
+
+    }
+}
